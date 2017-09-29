05e8eabf137c2e8d2e636d9690eee29f16d2a6e2
Ramaswamy P <email2rams@yahoo.com>
2017-09-29 17:30:07 -0500

    Create JobRolles.m

---
+++ JobRolles.m
@@ -0,0 +1,61 @@
+
+
+
+
+
+# DEVOPS SOLUTION ARCHITECT
+
+
+## RESPONSIBILITIES
+Design, architect, and implement next generation system architecture and automation solutions;
+Implement, maintain, and improve Continuous Integration and Continuous Delivery environments;
+Own and lead initiatives to define, design, and implement DevOps solutions which includes reference architectures, estimates, and costing;
+Provide technical leadership, project guidance, and business development in various mobile technology areas;
+Advise business and technology delivery leadership on how to translate the client’s infrastructure and automation business requirements into executable technology solutions;
+Participate in customer’s workshops and provide presentations of the proposed solution;
+Act as a subject matter expert on DevOps best practices with Cloud Formation, Auto Scaling Groups, and Configuration Management;
+Perform analysis best practices and emerging concepts in DevOps, Infrastructure Automation, and Enterprise Security;
+Acts as a technical liaison between clients, service engineering teams and support;
+Define common business and development processes, platform and tools usage for mobile solutions and delivery;
+Review and audit of existing solution, design and system architecture;
+Serve as a systems and automation evangelist providing thought leadership, participating in conferences, authoring white papers, etc;
+Perform profiling, troubleshooting of existing solutions;
+Create technical documentation.
+•	Serve as a champion/SME for Devops practices across the organization, providing coaching, mentoring, and technical support 
+•	Guide design and Implementation of Devops solutions 
+•	Bring new and emerging concepts in Devops to our organization 
+•	3+ Years of experience in a Development Operations organization leading, administering, building and maintaining an automation environment 
+•	Knowledge of Service Oriented Architectures 
+•	Track record of delivering applications and cloud-based infrastructure delivery in an agile environment 
+•	Experience with automated deployment, continuous integration, and release engineering tools 
+•	Knowledge of software development and software testing methodologies 
+•	Knowledge of cloud environments and how to implement cloud architectures 
+•	Knowledge of high availability, disaster recovery, auto-scaling and auto-healing concepts 
+•	Knowledge in various branching/merging methodologies and release procedures 
+•	Ability to communicate clearly and concisely and a flexible mindset to handle a quickly changing culture
+
+
+## REQUIREMENTS
+Broad knowledge of software development and software testing methodologies along with change and configuration management practices in Linux-based environments;
+Strong knowledge of cloud environments;
+Strong scripting skills (Python, Ruby, Perl);
+Experience with automated deployment, continuous integration, and release engineering tools (Nagios, Zabbix, Cacti, New Relic, Graphite);
+Strong knowledge of infrastructure automation tools (Puppet, Chef, Ansible);
+Working knowledge of Amazon Web Services (AWS);
+Prior consulting, advisory, or professional services experience is preferred;
+Demonstrated project management experience in Agile (Scrum), which includes project planning, execution, risk management, and resource management;
+Strong knowledge and expertise in DevOps solution delivery and strategy;
+Solid knowledge of design patterns and refactoring concepts;
+Practical expertise in performance tuning and optimization, bottleneck problems analysis;
+Active community member and contributor to open source projects is a plus;
+Solid technical expertise and troubleshooting skills;
+Possibility to travel ~50%;
+Fluent verbal and written English.
+•	Experience with cloud platforms (Azure, AWS, Cloud, etc.) 
+•	Experience with MS tool chains (.NET, VSO/VSTS, PowerShell) 
+•	Experience with Infrastructure as Code (ARM Templates/Terraform/Cloud Formation) 
+•	Experience with Continuous Integration/Continuous Deployment tools (Bamboo, VSTS, Team City) 
+•	Experience with Monitoring Tools (Splunk, ELK, DataDog) 
+•	Experience with Scripting (Powershell, Bash, Python, etc) 
+•	Experience with configuration management (Chef, Ansible, or Puppet) 
+•	Experience with Git based workflows
